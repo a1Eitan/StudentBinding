69e1550dfa8cff00eaab0811f8585092d319a212
eitan halili <halilieitan@gmail.com>
2026-05-20 01:56:38 +0300

    bvkjj

--- StudentBinding/MainPage.xaml.cs
+++ StudentBinding/MainPage.xaml.cs
@@ -1,12 +1,53 @@
-﻿namespace StudentBinding
+﻿using StudentBinding.Models;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace StudentBinding
 {
     public partial class MainPage : ContentPage
     {
-        
+        private Student currentStudent;
+        private Student s1;
+        private Student s2;
+
+        public Student CurrentStudent
+        {
+            get { return currentStudent; }
+            set { currentStudent = value; OnPropertyChanged(nameof(currentStudent)); }
+        }
         public MainPage()
         {
             InitializeComponent();
+
+            s1 = new Student("daniel.png", "daniel", 17, new DateTime(2009, 9, 1), "daniel@gmail.com", "0546973990");
+            s2 = new Student("noa.png", "noa", 16, new DateTime(2009, 8, 14), "noa@gmail.com", "0507200927");
+
+            CurrentStudent = s1;
+            editDate.Date = CurrentStudent.BirthDate;
+            this.BindingContext = CurrentStudent;
         }
 
+        private void ChangeStudent_Clicked(Object sender, EventArgs e)
+        {
+            if (CurrentStudent == s1)
+            {
+                CurrentStudent = s2;
+                editDate.Date = CurrentStudent.BirthDate;
+                this.BindingContext = CurrentStudent;
+            }
+            else
+            {
+                CurrentStudent = s1;
+                editDate.Date= CurrentStudent.BirthDate;
+                this.BindingContext = CurrentStudent;
+            }
+        }
+
+        private void UpdateDate_Clicked(object sender, EventArgs e)
+        {
+            CurrentStudent.BirthDate = editDate.Date;
+            this.BindingContext = null;
+            this.BindingContext = CurrentStudent;
+        }
     }
 }
---
+++ StudentBinding/Models/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentBinding.Models
+{
+    public class Student : ObservableObject
+    {
+        private string image;
+        private string name;
+        private int age;
+        private DateTime birthDate;
+        private string email;
+        private string phoneNumber;
+
+        public string Image
+        {
+            get { return image; }
+            set { image = value; OnPropertyChanged(nameof(Image)); }
+        }
+        public string Name { 
+            get { return name; } 
+            set { name = value;  OnPropertyChanged(nameof(Name)); } 
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; OnPropertyChanged(nameof(Age)); }
+        }
+        public DateTime BirthDate { 
+            get { return birthDate; } 
+            set { birthDate = value; OnPropertyChanged(nameof(BirthDate)); }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; OnPropertyChanged(nameof(Email)); }
+        }
+        public string PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; OnPropertyChanged(nameof(PhoneNumber)); }
+        }
+
+        public Student(string image, string name, int age, DateTime birthDate, string email, string phoneNumber)
+        {
+            this.image = image;
+            this.name = name;
+            this.age = age;
+            this.birthDate = birthDate;
+            this.email = email;
+            this.phoneNumber = phoneNumber;
+        }
+    }
+}
